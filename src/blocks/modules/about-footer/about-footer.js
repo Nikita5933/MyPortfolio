f39d661191c0c3f476541cfb6d1db6a53ad140b8
Nikita <nikita119@protonmail.com>
2023-12-19 16:00:56 +0300

    about-steps (about-page), added about-footer

---
+++ src/blocks/modules/about-footer/about-footer.js
@@ -0,0 +1,13 @@
+modules.define('about-footer', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
